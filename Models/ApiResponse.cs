5440f2ed2611a87890c52fccad3c74fe49546758
Muhammad Farid <40193232+muhmmdfrd@users.noreply.github.com>
2019-08-22 14:35:57 +0700

    Class for Api Response

---
+++ Models/ApiResponse.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace MiniMenu.Models
+{
+    public class ApiResponse<T> where T : class
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("messageCode")]
+        public int MessageCode { get; set; }
+
+        [JsonProperty("errorStatus")]
+        public bool ErrorStatus { get; set; }
+
+        [JsonProperty("errorCode")]
+        public int ErrorCode { get; set; }
+
+        [JsonProperty("data")]
+        public T Data { get; set; }
+
+        [JsonIgnore]
+        public long Id { get; set; }
+    }
+}
---
+++ Models/ApiResponseCheckOrder.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace MiniMenu.Models
+{
+    public class ApiResponseCheckOrder<T> where T : class
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("messageCode")]
+        public int MessageCode { get; set; }
+
+        [JsonProperty("errorStatus")]
+        public bool ErrorStatus { get; set; }
+
+        [JsonProperty("errorCode")]
+        public int ErrorCode { get; set; }
+
+        [JsonProperty("data")]
+        public T Data { get; set; }
+
+        [JsonProperty("orderId")]
+        public long Id { get; set; }
+    }
+}
