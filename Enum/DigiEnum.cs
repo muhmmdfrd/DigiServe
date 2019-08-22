b399c1271a7169f68c6e5dd31172643decfdde3a
Muhammad Farid <40193232+muhmmdfrd@users.noreply.github.com>
2019-08-22 14:38:05 +0700

    Core

---
+++ Enum/DigiEnum.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Enum
+{
+    public enum OrderState
+    {
+        Open = 0,
+        Start = 1,
+        Close = 2
+    }
+    public enum ChangeState
+    {
+        None = 0,
+        OnHold = 1,
+        OnComplete = 2
+    }
+}
